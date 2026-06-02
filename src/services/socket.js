6114deb1d673e4d8f80056f185df06637cbfb725
wizardhappy <142380854+happy0945@users.noreply.github.com>
2026-06-02 17:52:35 +0530

    Update socket.js

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,7 +1,7 @@
 
 import { io } from "socket.io-client";
 
-const WS_URL = import.meta.env.VITE_WS_URL || "http://localhost:5000";
+const WS_URL = import.meta.env.VITE_WS_URL || "https://stock-app-1-7imv.onrender.com";
 
 const socket = io(WS_URL, {
   autoConnect: false,
